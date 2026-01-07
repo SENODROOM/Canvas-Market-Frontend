d87469b3a7850b43ea4d785b1f65f98f4c1bb936
Javeria <yjaveria3@gmail.com>
2026-01-07 09:21:23 +0500

    Chuchu

--- src/MyComponents/Homepage.jsx
+++ src/MyComponents/Homepage.jsx
@@ -2,16 +2,20 @@ import { useEffect, useRef, useState } from "react";
 import mobile from '../Images/image1.jpg';
 import sources from '../Images/image2.jpg';
 import cards from '../Images/image3.jpg';
+import imago from '../Images/image5.jpg';
+import tango from '../Images/image6.jpg';
+import sango from '../Images/image7.jpg';
+import mango from '../Images/image8.jpg';
 import { Link } from 'react-router-dom';
 import { CircularImage, MonetizeSection } from "./MonetizeSection";
 const portfolioData = [
-    { id: 1, title: "Project One", description: "Description for project one.", image: mobile, tech: ["React", "CSS", "JS"] },
-    { id: 2, title: "Project Two", description: "Description for project two.", image: sources, tech: ["HTML", "CSS", "JS"] },
-    { id: 3, title: "Project Three", description: "Description for project three.", image: cards, tech: ["React", "Tailwind", "JS"] },
-    { id: 4, title: "Project Three", description: "Description for project three.", image: cards, tech: ["React", "Tailwind", "JS"] },
-    { id: 5, title: "Project Three", description: "Description for project three.", image: cards, tech: ["React", "Tailwind", "JS"] },
-    { id: 6, title: "Project Three", description: "Description for project three.", image: cards, tech: ["React", "Tailwind", "JS"] },
-    { id: 7, title: "Project Three", description: "Description for project three.", image: cards, tech: ["React", "Tailwind", "JS"] }
+    { id: 1, title: "Whispers of Nature", description: "-Muhammad Hassan", image: mobile, tech: ["Nature", "Illustration", "Minimal"] },
+    { id: 2, title: "Echoes of the Highlands", description: "-Ayesha Aqeel", image: sources, tech: ["Nature", "Serene", "Landscape Art"] },
+    { id: 3, title: "Textured Silence", description: "-Seemal Rubab", image: cards, tech: ["Abstract Art", "Modern", "Texture"] },
+    { id: 4, title: "Fading Petals", description: "-Khydija Fatima", image: tango, tech: ["Floral", "Calm Aesthetic", "Minimal Art"] },
+    { id: 5, title: "Masks of Emotion", description: "-Hafsa Amin", image: imago, tech: ["Expressionism", "Theatre Masks", "Abstract Art"] },
+    { id: 6, title: "City After Dusk", description: "-Javeria Yasin", image: sango, tech: ["Urban Canvas", "Fine Art", "Moody"] },
+    { id: 7, title: "Silent Majesty", description: "-Muhammad Saad Amin", image: mango, tech: ["Concept Art", "Portrait", "Canvas"] }
 ];
 
 function Homepage() {
