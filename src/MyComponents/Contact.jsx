78364bdaea31c763862d15b48b3f8b81c8a90a5d
Javeria <yjaveria3@gmail.com>
2026-01-10 02:31:42 +0500

    contact

--- src/MyComponents/Contact.jsx
+++ src/MyComponents/Contact.jsx
@@ -66,7 +66,7 @@ function Contact() {
                 <form method="POST" >
                     <h1 className="formh1">Contact Us</h1>
                     <div id="form">
-                        <p className="formh1">Let us know how we can help you and our team will be in touch as soon as possible!</p>
+                        <p >Let us know how we can help you and our team will be in touch as soon as possible!</p>
                         <div className="formdesign" id="name">
                             <input type="text" name="fname" required placeholder="Your Name" className="place" onChange={handleInputs} /><b><span
                                 className="formerror"></span></b>
@@ -76,7 +76,7 @@ function Contact() {
                                 className="formerror"></span></b>
                         </div>
                         <div className="formdesign" id="msg">
-                            <input type="message" name="fmsg" required placeholder="Message`" className="place" onChange={handleInputs} /> <b><span
+                            <input type="message" name="fmsg" required placeholder="Message" className="place" onChange={handleInputs} /> <b><span
                                 className="formerror"></span></b>
                         </div>
                         <button className="contact-but but" type="submit" value="Send Message" onClick={PostData}>Send Message</button>
@@ -88,23 +88,7 @@ function Contact() {
 
             </div>
 
-            <div className="follow">
-                <h1>Follow Us</h1>
-                <br />
-                <p>Follow us on our social channels</p>
-                <br />
-                <ul>
-                    <li><a href="#"><img src={facebooklogo} alt="" /></a></li>
-                    <li><a href="#"><img src={twitterlogo} alt="" /></a></li>
-                    <li><a href="#"><img src={instalogo} alt="" /></a></li>
-                    <li><a href="#"><img src={pinlogo} alt="" /></a></li>
-                    <li><a href="#"><img src={mediumlogo} alt="" /></a></li>
-                    <li><a href="#"><img src={linklogo} alt="" /></a></li>
-                </ul>
-                <br />
-                <p>MuzliX Ltd. | Tel-Aviv, Israel | <a href="#">info@muz.li </a></p>
-            </div>
-
+            
         </main>
     )
 }
