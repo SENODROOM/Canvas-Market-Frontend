ba1fb46437b9152efaae91bc2991d49fb417e91f
Muhammad Saad Amin <saadamin691@gmail.com>
2026-01-25 01:30:22 +0500

    OOP

---
+++ src/Data/Database.js
@@ -0,0 +1,23 @@
+import mobile from '../Images/image1.jpg';
+import sources from '../Images/image2.jpg';
+import cards from '../Images/image3.jpg';
+import imago from '../Images/image5.jpg';
+import tango from '../Images/image6.jpg';
+import sango from '../Images/image7.jpg';
+import mango from '../Images/image8.jpg';
+
+export const portfolioData = [
+    {
+        id: 1,
+        title: "Whispers of Nature",
+        description: "-Muhammad Hassan",
+        image: mobile,
+        tech: ["Nature", "Illustration", "Minimal"]
+    },
+    { id: 2, title: "Echoes of the Highlands", description: "-Ayesha Aqeel", image: sources, tech: ["Nature", "Serene", "Landscape Art"] },
+    { id: 3, title: "Textured Silence", description: "-Seemal Rubab", image: cards, tech: ["Abstract Art", "Modern", "Texture"] },
+    { id: 4, title: "Fading Petals", description: "-Khydija Fatima", image: tango, tech: ["Floral", "Calm Aesthetic", "Minimal Art"] },
+    { id: 5, title: "Masks of Emotion", description: "-Hafsa Amin", image: imago, tech: ["Expressionism", "Theatre Masks", "Abstract Art"] },
+    { id: 6, title: "City After Dusk", description: "-Javeria Yasin", image: sango, tech: ["Urban Canvas", "Fine Art", "Moody"] },
+    { id: 7, title: "Silent Majesty", description: "-Muhammad Saad Amin", image: mango, tech: ["Concept Art", "Portrait", "Canvas"] }
+];
--- src/MyComponents/Slider.jsx
+++ src/MyComponents/Slider.jsx
@@ -1,24 +1,7 @@
 import { useEffect, useRef, useState } from "react";
-import mobile from '../Images/image1.jpg';
-import sources from '../Images/image2.jpg';
-import cards from '../Images/image3.jpg';
-import imago from '../Images/image5.jpg';
-import tango from '../Images/image6.jpg';
-import sango from '../Images/image7.jpg';
-import mango from '../Images/image8.jpg';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { portfolioData } from "../Data/Database";
 export function Sliders() {
-
-  const portfolioData = [
-    { id: 1, title: "Whispers of Nature", description: "-Muhammad Hassan", image: mobile, tech: ["Nature", "Illustration", "Minimal"] },
-    { id: 2, title: "Echoes of the Highlands", description: "-Ayesha Aqeel", image: sources, tech: ["Nature", "Serene", "Landscape Art"] },
-    { id: 3, title: "Textured Silence", description: "-Seemal Rubab", image: cards, tech: ["Abstract Art", "Modern", "Texture"] },
-    { id: 4, title: "Fading Petals", description: "-Khydija Fatima", image: tango, tech: ["Floral", "Calm Aesthetic", "Minimal Art"] },
-    { id: 5, title: "Masks of Emotion", description: "-Hafsa Amin", image: imago, tech: ["Expressionism", "Theatre Masks", "Abstract Art"] },
-    { id: 6, title: "City After Dusk", description: "-Javeria Yasin", image: sango, tech: ["Urban Canvas", "Fine Art", "Moody"] },
-    { id: 7, title: "Silent Majesty", description: "-Muhammad Saad Amin", image: mango, tech: ["Concept Art", "Portrait", "Canvas"] }
-  ];
-
 
   const carouselRef = useRef(null);
   const indicatorsRef = useRef(null);
